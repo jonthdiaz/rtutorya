3ef71be0f004509732f2e4cdffc8ed54989a290c
Jonathan diaz <jonthdiaz@gmail.com>
2015-11-09 17:34:37 -0500

    Update query.R

--- query.R
+++ query.R
@@ -8,20 +8,20 @@ users = dbGetQuery(con,"select * from profiles_user")
 students = dbGetQuery(con,"select * from profiles_student")
 tutors = dbGetQuery(con,"select * from profiles_tutor")
 payments = dbGetQuery(con, "select * from billing_pagosonlinepayment ")
-
-head(users)
-str(users)
-summary(users$id)
-plot(users$id)
-plot(payments$transaction_number)
-
-barplot(payments$payment_ptr_id)
-
-# exporta informacion a excel
-# write.table(payments, "prueba.xls", sep="\t")
+groups = dbGetQuery(con, "select * from courses_coursegroupstudent")
+groups_course = dbGetQuery(con, "select * from courses_coursegroupstudent_courses")
+groups_student =dbGetQuery(con, "select * from courses_coursegroupstudent_students")
+users_student <- merge(students, users, by.x = "user_id",by.y = "id")
+courses_groups <- merge(groups_student,groups, by.x = "coursegroupstudent_id", by.y = "id")
+courses_groups_student <- merge(courses_groups, users_student, by.x="student_id", by.y = "id")
 
 
-#tt = users[users$username=="jefferson.sur@hotmail.com",]
+sort(courses_groups_student$coursegroupstudent_id)
+# student class
+student_class <- dbGetQuery(con,"select * from courses_coursestudentclass")
+courses_groups_student$student_id
+classes <- merge(student_class, courses_groups_student,by.x="student_id", by.y="student_id")
 
-# Disconnect db
-#dbDisconnect(con)
+
+
+
